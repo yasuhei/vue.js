514289ffeefdc4729a1b5e454d1c1052df7e953d
Yasuhei13 <67479901+yasuhei@users.noreply.github.com>
2023-02-05 21:24:21 -0300

    first commit

---
+++ todo/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <TheHeader />
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from './components/TheHeader.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    TheHeader
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
